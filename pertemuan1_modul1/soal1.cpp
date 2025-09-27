374a9847ec1c09763348e672a940e873c5195b43
Mutiara Fauziah <mutiarafauziah@MacBook-Air-Mutiara.local>
2025-09-27 18:45:21 +0700

    first commit

--- pertemuan1_modul1/soal1.cpp
+++ pertemuan1_modul1/soal1.cpp
@@ -1,17 +1,4 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nama;
-    int umur;
 
-    cout << "Masukkan nama: ";
-    cin >> nama;
-
-    cout << "Masukkan umur: ";
-    cin >> umur;
-
-    cout << "Halo, " << nama << "! Umur kamu " << umur << " tahun." << endl;
-
-    return 0;
-}
