0f654f411871ce87fcc6bb919629ccb9fd1f8f41
mutiara1904 <mutiarafauziah161@gmail.com>
2025-09-27 22:31:50 +0700

    Update soal1.cpp

--- pertemuan1_modul1/soal1.cpp
+++ pertemuan1_modul1/soal1.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 using namespace std;
+nt main(){
+    float x, y;
+    cout << "Masukan bilangan pertama : ";
+    cin >> x;
+    cout << "Masukan bilangan kedua : ";
+    cin >> y;
+
+    cout << "Penjumlahan : ";
+    cout  << x + y << endl;
+    cout << "Pengurangan : " ;
+    cout << x - y << endl;
+    cout << "Perkalian : ";
+    cout  << x * y << endl;
+    cout << "Pembagian : ";
+    cout << x / y << endl;
+    return 0;
+}
+
 
 
